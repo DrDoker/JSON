22b7fc8d1546c5b69254ec46d3262f271315688e
Serhii Tkachenko <doker.tk@gmail.com>
2022-10-30 18:22:41 +0100

    fix

--- JSON HW.playground/Sources/GetData.swift
+++ JSON HW.playground/Sources/GetData.swift
@@ -1,12 +1,12 @@
 import Foundation
 
 public struct GetData {
-    public static func allCards(urlReuaest: URL?) {
+    public static func allCards(urlReuaest: URL?, cardsName: [String]) {
         guard let url = urlReuaest else { return }
         URLSession.shared.jsonDecodableTask(with: url) { (result: Result<Cards, Error>) in
             switch result {
-            case .success(let cards):
-                cards.printAllCards()
+            case .success(var cards):
+                cards.printAllCards(cardsName)
             case .failure(let error):
                 print("Error: \(error.localizedDescription)")
             }
